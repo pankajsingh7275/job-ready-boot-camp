bc0f04e31dea8268ba7c3dda262de487efd2b3d4
pankajsingh7275 <110216514+pankajsingh7275@users.noreply.github.com>
2022-08-02 23:37:04 +0530

    Add files via upload

---
+++ asst1q11.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int h,m;
+    printf("input time in the format HH:MM  ");
+    scanf("%d:%d", &h,&m);
+    printf("%d HOUR AND %d MINUTE",h,m);
+    getch();
+
+}
