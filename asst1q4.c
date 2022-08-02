3d4476dd0b4905bea9fa8cc6bba5e6af714afde1
pankajsingh7275 <110216514+pankajsingh7275@users.noreply.github.com>
2022-08-02 22:27:35 +0530

    Add files via upload

---
+++ asst1q4.c
@@ -0,0 +1,14 @@
+ #include<stdio.h>
+ #include<conio.h>
+ int main()
+ {
+     float R,A;
+     printf("Enter the radius of circle");
+     scanf("%f", &R);
+     A=3.14*R*R;
+     printf("Ara of the circlr is %f having the radius %f", A,R);
+     getch();
+
+
+
+ }
